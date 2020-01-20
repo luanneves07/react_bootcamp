14526174ec3a0c54eb00898e09725946b35aaf96
luanneves07 <luannevessilva@gmail.com>
2020-01-20 04:32:10 -0300

    RJS-0000 - IssueList
    - Configura lista de issues
    - Estiliza lista

--- modulo05/src/components/Container/index.js
+++ modulo05/src/components/Container/index.js
@@ -5,7 +5,7 @@ const Container = styled.div`
   background: #fff;
   border-radius: 4px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 80px;
+  padding: 30px;
   margin: 80px auto;
 
   h1 {
